0d4dd9cbee3f2448bad568fda65b722c94767e34
duemaster <zhangbsn@gmail.com>
2019-01-05 18:25:38 +0800

    update

--- glassfish/src/browser/glassfish-contribution.ts
+++ glassfish/src/browser/glassfish-contribution.ts
@@ -1,23 +1,60 @@
 import { injectable, inject } from "inversify";
-import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry, MessageService } from "@theia/core/lib/common";
-import { CommonMenus } from "@theia/core/lib/browser";
+import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry, MessageService, MAIN_MENU_BAR, Command } from "@theia/core/lib/common";
 
-export const GlassFishExtensionCommand = {
-    id: 'GlassFishExtension.command',
-    label: "Shows a message"
-};
+export namespace GlassFishMenu {
+    //Some Magic String concerning placement order in menu
+    const glassFishString = "5_glassfish";
+
+    export const GlassFish = [...MAIN_MENU_BAR, glassFishString];
+    export const GlassFish_START = [...GlassFish, glassFishString];
+    export const GlassFish_STOP = [...GlassFish, glassFishString];
+}
+
+export namespace GlassFishCommands {
+    const GLASSFISH_CATEGORY = "GlassFish"
+
+    export const START_SERVER: Command = {
+        id: 'glassfish:start',
+        category: GLASSFISH_CATEGORY,
+        label: 'Start GlassFish Server'
+    }
+
+    export const STOP_SERVER: Command = {
+        id: 'glassfish:stop',
+        category: GLASSFISH_CATEGORY,
+        label: 'Stop GlassFish Server'
+    }
+}
 
 @injectable()
 export class GlassFishExtensionCommandContribution implements CommandContribution {
+
+    //Need to Save in Cookie Later + Periodic Refresh
+    isServerStarted: boolean = false;
 
     constructor(
         @inject(MessageService) private readonly messageService: MessageService,
     ) { }
 
     registerCommands(registry: CommandRegistry): void {
-        registry.registerCommand(GlassFishExtensionCommand, {
-            execute: () => this.messageService.info('Hello World!')
-        });
+
+        registry.registerCommand(GlassFishCommands.START_SERVER, {
+            execute: async () => {
+                this.messageService.info("Starting GlassFish Server")
+                this.isServerStarted = true;
+                this.messageService.info("GlassFish Server Started Successfully")
+            },
+            isEnabled: () => !this.isServerStarted
+        })
+
+        registry.registerCommand(GlassFishCommands.STOP_SERVER, {
+            execute: () => {
+                this.messageService.info("Stopping GlassFish Server");
+                this.isServerStarted = false;
+                this.messageService.info("GlassFish Server Stopped Successfully")
+            },
+            isEnabled: () => this.isServerStarted
+        })
     }
 }
 
@@ -25,9 +62,20 @@ export class GlassFishExtensionCommandContribution implements CommandContributio
 export class GlassFishExtensionMenuContribution implements MenuContribution {
 
     registerMenus(menus: MenuModelRegistry): void {
-        menus.registerMenuAction(CommonMenus.EDIT_FIND, {
-            commandId: GlassFishExtensionCommand.id,
-            label: 'Say Hello'
+
+        menus.registerSubmenu(GlassFishMenu.GlassFish, "GlassFish");
+
+        menus.registerMenuAction(GlassFishMenu.GlassFish_START, {
+            commandId: GlassFishCommands.START_SERVER.id,
+            label: 'Start GlassFish Server',
+            order: '0'
         });
+
+        menus.registerMenuAction(GlassFishMenu.GlassFish_STOP, {
+            commandId: GlassFishCommands.STOP_SERVER.id,
+            label: 'Stop GlassFish Server',
+            order: '1'
+        })
+
     }
 }
