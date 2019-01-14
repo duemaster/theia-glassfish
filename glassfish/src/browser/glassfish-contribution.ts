8cd08a7465c748eec5d1a91dbf2462af95a56a54
duemaster <zhangbsn@gmail.com>
2019-01-14 18:03:12 +0800

    update scripts

--- glassfish/src/browser/glassfish-contribution.ts
+++ glassfish/src/browser/glassfish-contribution.ts
@@ -1,17 +1,23 @@
 import { injectable, inject } from "inversify";
-import { 
-    CommandContribution, 
-    CommandRegistry, 
-    MenuContribution, 
-    MenuModelRegistry, 
-    MessageService, 
+import {
+    CommandContribution,
+    CommandRegistry,
+    MenuContribution,
+    MenuModelRegistry,
+    MessageService,
     MAIN_MENU_BAR,
     SelectionService,
-    Command 
+    Command,
+    CommandService
 } from "@theia/core/lib/common";
 import URI from "@theia/core/lib/common/uri";
 import { UriAwareCommandHandler } from '@theia/core/lib/common/uri-command-handler';
 import { FileSystem } from '@theia/filesystem/lib/common';
+import { TerminalService } from '@theia/terminal/lib/browser/base/terminal-service';
+import { MiniBrowserOpenHandler } from "@theia/mini-browser/lib/browser/mini-browser-open-handler"
+import { FrontendApplicationStateService } from "@theia/core/lib/browser/frontend-application-state";
+import { FrontendApplicationContribution, FrontendApplication } from "@theia/core/lib/browser";
+
 
 export namespace GlassFishMenu {
     //Some Magic String concerning placement order in menu
@@ -20,8 +26,9 @@ export namespace GlassFishMenu {
     export const GlassFish = [...MAIN_MENU_BAR, glassFishString];
     export const GlassFish_START = [...GlassFish, glassFishString];
     export const GlassFish_STOP = [...GlassFish, glassFishString];
+    export const GlassFish_BUILD = [...GlassFish, glassFishString];
 
-    export const GlassFish_NAVIGATOR_CONTEXT_MENU = ['navigator-context-menu', "5_glassfish"]
+    export const GlassFish_NAVIGATOR_CONTEXT_MENU = ['navigator-context-menu', glassFishString]
 }
 
 export namespace GlassFishCommands {
@@ -62,10 +69,22 @@ export namespace GlassFishCommands {
         category: GLASSFISH_CATEGORY,
         label: 'Undeploy .jar file'
     }
+
+    export const ADD_JDBC_RESOURCE: Command = {
+        id: 'glassfish:add_resource',
+        category: GLASSFISH_CATEGORY,
+        label: 'Add JDBC Resources'
+    }
+
+    export const BUILD_PROJECT: Command = {
+        id: 'glassfish:build_project',
+        category: GLASSFISH_CATEGORY,
+        label: 'Build Project'
+    }
 }
 
 @injectable()
-export class GlassFishExtensionCommandContribution implements CommandContribution {
+export class GlassFishExtensionCommandContribution implements CommandContribution, FrontendApplicationContribution {
 
     //Need to Save in Cookie Later + Periodic Refresh
     isServerStarted: boolean = false;
@@ -73,23 +92,66 @@ export class GlassFishExtensionCommandContribution implements CommandContributio
     constructor(
         @inject(MessageService) private readonly messageService: MessageService,
         @inject(FileSystem) protected readonly fileSystem: FileSystem,
-        @inject(SelectionService) protected readonly selectionService: SelectionService
+        @inject(SelectionService) protected readonly selectionService: SelectionService,
+        @inject(TerminalService) private readonly terminalService: TerminalService,
+        @inject(MiniBrowserOpenHandler) protected readonly openHandler: MiniBrowserOpenHandler,
+        @inject(FrontendApplicationStateService) private readonly stateService: FrontendApplicationStateService,
+        @inject(CommandService) private readonly commandService: CommandService
     ) { }
+
+    async onStart(app: FrontendApplication) {
+        this.stateService.reachedState('ready').then(
+            async (a) => {
+                //Start Server
+                await this.commandService.executeCommand(GlassFishCommands.START_SERVER.id);
+                //Add JDBC
+                await this.commandService.executeCommand(GlassFishCommands.BUILD_PROJECT.id);
+                //Build Project
+                // await this.commandService.executeCommand(GlassFishCommands.ADD_JDBC_RESOURCE.id);
+                //Deploy Project
+                // await this.commandService.executeCommand(GlassFishCommands.DEPL.id);
+
+                // this.openHandler.openPreview("localhost:8080//SimpleBlog-war/");
+            }
+        )
+    }
 
     registerCommands(registry: CommandRegistry): void {
 
         registry.registerCommand(GlassFishCommands.START_SERVER, {
             execute: async () => {
-                this.messageService.info("Starting GlassFish Server")
+                this.messageService.info("Starting GlassFish Server");
+
+                const terminalWidget = await this.terminalService.newTerminal({});
+                await terminalWidget.start();
+                await this.terminalService.activateTerminal(terminalWidget);
+
+                await new Promise(resolve => {
+                    setTimeout(resolve, 1000);
+                })
+
+                await terminalWidget.sendText("asadmin start-domain --verbose && exit 1\n")
+
                 this.isServerStarted = true;
-                this.messageService.info("GlassFish Server Started Successfully")
+                this.messageService.info("GlassFish Server Started Successfully");
             },
             isEnabled: () => !this.isServerStarted
         })
 
         registry.registerCommand(GlassFishCommands.STOP_SERVER, {
-            execute: () => {
+            execute: async () => {
                 this.messageService.info("Stopping GlassFish Server");
+
+                const terminalWidget = await this.terminalService.newTerminal({});
+                await terminalWidget.start();
+                await this.terminalService.activateTerminal(terminalWidget);
+
+                await new Promise(resolve => {
+                    setTimeout(resolve, 1000);
+                })
+
+                await terminalWidget.sendText("asadmin stop-domain && exit 1\n")
+
                 this.isServerStarted = false;
                 this.messageService.info("GlassFish Server Stopped Successfully")
             },
@@ -119,23 +181,80 @@ export class GlassFishExtensionCommandContribution implements CommandContributio
 
         registry.registerCommand(GlassFishCommands.DEPLOY_WAR, new UriAwareCommandHandler<URI>(this.selectionService, {
             execute: async uri => {
-                const stat = await this.fileSystem.getFileStat(uri.toString());
-                this.messageService.info("Opening: " + stat);
+                const terminalWidget = await this.terminalService.newTerminal({});
+                await terminalWidget.start();
+                await this.terminalService.activateTerminal(terminalWidget);
+
+                await new Promise(resolve => {
+                    setTimeout(resolve, 1000);
+                })
+
+                await terminalWidget.sendText(`asadmin deploy --force ${uri.path} || \\bin\\true && exit 1\n`);
+
+                await new Promise(resolve => {
+                    setTimeout(resolve, 1000);
+                })
+
+                //Open Browser to target URL
+                this.openHandler.openPreview("localhost:8080//SimpleBlog-war/");
+                this.messageService.info("Application Deployed Successfully");
             },
             isVisible: uri => {
-                return uri.path.ext == '.war'
+                return uri.path.ext == '.war';
             }
         }))
 
         registry.registerCommand(GlassFishCommands.UNDEPLOY_WAR, new UriAwareCommandHandler<URI>(this.selectionService, {
             execute: async uri => {
-                const stat = await this.fileSystem.getFileStat(uri.toString());
-                this.messageService.info("Undeploy: " + stat);
+                const terminalWidget = await this.terminalService.newTerminal({});
+                await terminalWidget.start();
+                await this.terminalService.activateTerminal(terminalWidget);
+
+                await new Promise(resolve => {
+                    setTimeout(resolve, 1000);
+                })
+
+                await terminalWidget.sendText(`asadmin undeploy ${uri.path} && exit 1\n`);
             },
             isVisible: uri => {
-                return uri.path.ext == '.war'
+                //return !uri.path.ext && uri.path.toString().endsWith(".war");
+                return uri.path.ext == '.war';
             }
         }))
+
+        registry.registerCommand(GlassFishCommands.ADD_JDBC_RESOURCE, new UriAwareCommandHandler<URI>(this.selectionService, {
+            execute: async uri => {
+                // Deploy JDBC Resources
+                const terminalWidget = await this.terminalService.newTerminal({});
+                await terminalWidget.start();
+                await this.terminalService.activateTerminal(terminalWidget);
+
+                await new Promise(resolve => {
+                    setTimeout(resolve, 1000);
+                })
+
+                await terminalWidget.sendText(`asadmin add-resources ${uri.path} || \\bin\\true && exit 1\n`)
+            },
+            isVisible: uri => {
+                return uri.path.ext == '.xml';
+            }
+        }))
+
+        registry.registerCommand(GlassFishCommands.BUILD_PROJECT, {
+            execute: async () => {
+                // Deploy Build Project
+                const terminalWidget = await this.terminalService.newTerminal({});
+                await terminalWidget.start();
+                await this.terminalService.activateTerminal(terminalWidget);
+
+                await new Promise(resolve => {
+                    setTimeout(resolve, 1000);
+                })
+
+                await terminalWidget.sendText(`bash ./gradlew build\n`)
+            },
+            isEnabled: () => true
+        })
     }
 }
 
@@ -158,6 +277,13 @@ export class GlassFishExtensionMenuContribution implements MenuContribution {
             order: '1'
         })
 
+        menus.registerMenuAction(GlassFishMenu.GlassFish_BUILD, {
+            commandId: GlassFishCommands.BUILD_PROJECT.id,
+            label: 'Build Project',
+            order: '2'
+        })
+
+
         menus.registerMenuAction(GlassFishMenu.GlassFish_NAVIGATOR_CONTEXT_MENU, {
             commandId: GlassFishCommands.DEPLOY_JAR.id
         })
@@ -172,6 +298,10 @@ export class GlassFishExtensionMenuContribution implements MenuContribution {
 
         menus.registerMenuAction(GlassFishMenu.GlassFish_NAVIGATOR_CONTEXT_MENU, {
             commandId: GlassFishCommands.UNDEPLOY_WAR.id
+        })
+
+        menus.registerMenuAction(GlassFishMenu.GlassFish_NAVIGATOR_CONTEXT_MENU, {
+            commandId: GlassFishCommands.ADD_JDBC_RESOURCE.id
         })
 
     }
--- glassfish/src/browser/glassfish-frontend-module.ts
+++ glassfish/src/browser/glassfish-frontend-module.ts
@@ -7,12 +7,17 @@ import {
     CommandContribution,
     MenuContribution
 } from "@theia/core/lib/common";
+import { FrontendApplicationContribution } from '@theia/core/lib/browser';
 
 import { ContainerModule } from "inversify";
 
 export default new ContainerModule(bind => {
     // add your contribution bindings here
-    
+
     bind(CommandContribution).to(GlassFishExtensionCommandContribution);
     bind(MenuContribution).to(GlassFishExtensionMenuContribution);
+
+    //Add in Application Lifecycle
+    bind(GlassFishExtensionCommandContribution).toSelf().inSingletonScope();
+    bind(FrontendApplicationContribution).toService(GlassFishExtensionCommandContribution);
 });
